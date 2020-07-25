e3c7bb68f151f2af428334deb645a6b39666f414
zzq <1402806685@qq.com>
2020-07-25 20:25:47 +0800

    字典序的第K小数字 我好像用了什么特别复杂的方法。。（数位dp） https://leetcode-cn.com/problems/k-th-smallest-in-lexicographical-order/

--- main.cpp
+++ main.cpp
@@ -7,36 +7,73 @@
 typedef long long LL;
 using namespace std;
 const int MAX = 40 + 5;
-LL dp[MAX];
+LL dp[MAX][3];
 int shu[MAX];
 int ans[MAX];
-int n = 0, k = 0; 
+int n = 0, k = 0;
 bool finish;
-int dfs(int len, int res, bool isin, bool allzero,  bool limit)
+int icnt = 0;
+int dfs(int len, int res, bool isin, int limit)
 {
-	if (len == -1)
+	if (res == 1)
 	{
-		if (res == 1 && isin==false)
-			finish = true;
+		finish = true;
+		return 0;
+	}
+	if ((limit == 2 && len == 0) || len == -1)
+	{
 		return 1;
 	}
-
-	if (limit == false && dp[len] != -1 && isin==false)
-		return dp[len];
-
-	int cnt = 0;
+	if(dp[len][limit] != -1 && isin == false)
+		return dp[len][limit];
 	int c = 0;
-	for (int i = 0; i <= (limit ? shu[len] : 9); i++)
+	int cnt = 1;
+	for (int i = ((len == icnt - 1) ? 1 : 0); i <= (limit == 1 && len == 0 ? shu[len] : 9); i++)
 	{
 		ans[len] = i;
-		c = dfs(len - 1, res - cnt, false, allzero && i==0, limit && i == shu[len]);
-		if(cnt + c>=res)
-			dfs(len - 1, res - cnt, true, allzero && i==0, limit && i == shu[len]);
-		cnt += c;
+		if (limit == 0 ||(limit ==1 &&i < shu[len]))
+			c = dfs(len - 1, res - cnt, false, 0);
+		else if (limit == 1 && i == shu[len])
+			c = dfs(len - 1, res - cnt, false, 1);
+		else if (limit == 2 || (limit == 1 &&  i > shu[len]))
+			c = dfs(len - 1, res - cnt, false, 2);
 
-		if (finish) return 0;
+		if (cnt + c >= res)
+		{
+			if (limit == 0 ||(limit ==1 &&i < shu[len]))
+				c = dfs(len - 1, res - cnt, true, 0);
+			else if (limit == 1 && i == shu[len])
+				c = dfs(len - 1, res - cnt, true, 1);
+			else if (limit == 2|| (limit == 1 &&  i > shu[len]))
+				c = dfs(len - 1, res - cnt, true, 2);
+		}
+		cnt += c;
+		if (finish == true)
+			return 0;
+		ans[len] = -1;
 	}
-	return  limit ? cnt : dp[len] = cnt;
+	return dp[len][limit] = cnt;
+}
+int findKthNumber(int n, int k)
+{
+	int ans2 = 0;
+		k +=1;
+		finish = false;
+		mem(dp, -1);
+		mem(ans, -1);
+		icnt = 0;
+		while (n)
+		{
+			shu[icnt++] = n % 10;
+			n /= 10;
+		}
+
+		dfs(icnt - 1, k, true, 1);
+		for (int i = icnt - 1; i >= 0; i--)
+			if (ans[i] != -1)
+				ans2 *= 10, ans2 += ans[i];
+
+				return ans2;
 }
 int main()
 {
@@ -45,20 +82,7 @@ int main()
 #endif // _local
 	while(cin >> n >> k)
 	{
-		k += 1;
-		finish = false;
-		mem(dp, -1);
-		int cnt = 0;
-		while (n)
-		{
-			shu[cnt++] = n % 10;
-			n /= 10;
-		}
-
-		dfs(cnt - 1, k, true, true, true);
-		for (int i = cnt -1; i >= 0; i--)
-			printf("%d", ans[i]);
-		cout << endl;
+		cout << findKthNumber(n, k) << endl;
 	}
 	return 0;
 }
